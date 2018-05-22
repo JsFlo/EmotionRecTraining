f56f55fefef41e6be925d2f966fad25fac823a9d
Jose Flores <jsflores480@gmail.com>
2018-05-21 20:00:07 -0500

    Adds emotion train script

--- emotionRecTrain.py
+++ emotionRecTrain.py
@@ -17,6 +17,8 @@ from tensorflow.python.saved_model.signature_def_utils_impl import build_signatu
 from tensorflow.contrib.session_bundle import exporter
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--csv_file', type=str, required=True)
+parser.add_argument('--export_path', type=str, required=True)
 # OPTIONAL
 parser.add_argument('--batch_size', type=int, default=1)
 parser.add_argument('--n_epochs', type=int, default=1)
@@ -28,17 +30,19 @@ if (FLAGS.debug):
     FLAGS.batch_size = 10
     FLAGS.n_epochs = 1
 
-# todo argparse
-raw_data_csv_file_name = 'data/fer2013.csv'
+NUM_CLASSES=7
+IMG_SIZE = 48
+
+#TODO: Use the 'Usage' field to separate based on training/testing
 TRAIN_END = 28708
 TEST_START = TRAIN_END + 1
-IMG_SIZE = 48
+
 
 
 def split_for_test(list):
-    first = list[0:TRAIN_END]
-    second = list[TEST_START:]
-    return first, second
+    train = list[0:TRAIN_END]
+    test = list[TEST_START:]
+    return train, test
 
 
 def pandas_vector_to_list(pandas_df):
@@ -47,35 +51,53 @@ def pandas_vector_to_list(pandas_df):
 
 
 def process_emotion(emotion):
+    """
+    Takes in a vector of emotions and outputs a list of emotions as one-hot vectors.
+    :param emotion: vector of ints (0-7)
+    :return: list of one-hot vectors (array of 7)
+    """
     emotion_as_list = pandas_vector_to_list(emotion)
-    y_data = []
-    for index in range(len(emotion_as_list)):
-        y_data.append(emotion_as_list[index])
-
-    # Y data
-    y_data_categorical = np_utils.to_categorical(y_data, NUM_CLASSES)
+    # convert to one hots
+    y_data_categorical = np_utils.to_categorical(emotion_as_list, NUM_CLASSES)
     return y_data_categorical
 
 
 def process_pixels(pixels, img_size=IMG_SIZE):
+    """
+    Takes in a string (pixels) that has space separated ints. Will transform the ints
+    to a 48x48 matrix of floats(/255).
+    :param pixels: string with space separated ints
+    :param img_size: image size
+    :return: array of 48x48 matrices
+    """
     pixels_as_list = pandas_vector_to_list(pixels)
+
     np_image_array = []
     for index, item in enumerate(pixels_as_list):
+        # 48x48
         data = np.zeros((img_size, img_size), dtype=np.uint8)
+        # split space separated ints
         pixel_data = item.split()
+
+        # 0 -> 47, loop through the rows
         for i in range(0, img_size):
+            # (0 = 0), (1 = 47), (2 = 94), ...
             pixel_index = i * img_size
+            # (0 = [0:47]), (1 = [47: 94]), (2 = [94, 141]), ...
             data[i] = pixel_data[pixel_index:pixel_index + img_size]
+
         np_image_array.append(np.array(data))
+
     np_image_array = np.array(np_image_array)
+    # convert to float and divide by 255
     np_image_array = np_image_array.astype('float32') / 255.0
     return np_image_array
 
 
-def get_vg_feature_map(vg, array_input, n_feature_maps):
+def get_vgg16_output(vgg16, array_input, n_feature_maps):
     vg_input = duplicate_input_layer(array_input, n_feature_maps)
 
-    picture_train_features = vg.predict(vg_input)
+    picture_train_features = vgg16.predict(vg_input)
     del (vg_input)
 
     feature_map = np.empty([n_feature_maps, 512])
@@ -94,76 +116,86 @@ def duplicate_input_layer(array_input, size):
 
 
 def main():
+    # used to get the session/graph data from keras
     K.set_learning_phase(0)
-    raw_data = pd.read_csv(raw_data_csv_file_name)
+    # get the data in a Pandas dataframe
+    raw_data = pd.read_csv(FLAGS.csv_file)
+    raw_data = raw_data[0:2]
+
+    # convert to one hot vectors
     emotion_array = process_emotion(raw_data[['emotion']])
+    # convert to a 48x48 float matrix
     pixel_array = process_pixels(raw_data[['pixels']])
+
+    # split for test/train
     y_train, y_test = split_for_test(emotion_array)
     x_train_matrix, x_test_matrix = split_for_test(pixel_array)
+
     n_train = int(len(x_train_matrix))
     n_test = int(len(x_test_matrix))
+
     x_train_input = duplicate_input_layer(x_train_matrix, n_train)
     x_test_input = duplicate_input_layer(x_test_matrix, n_test)
 
-    # vgg 16. include_top=False so the output is the 512
+    # vgg 16. include_top=False so the output is the 512 and use the learned weights
     vg = VGG16(include_top=False, input_shape=(48, 48, 3), weights='imagenet')
 
-    # get feature map
-    x_train_feature_map = get_vg_feature_map(vg, x_train_matrix, n_train)
-    x_test_feature_map = get_vg_feature_map(vg, x_test_matrix, n_test)
+    # get vgg16 outputs
+    x_train_feature_map = get_vgg16_output(vg, x_train_matrix, n_train)
+    x_test_feature_map = get_vgg16_output(vg, x_test_matrix, n_test)
 
-    # very important to do this as a first thing
-
-    # with tf.device('/gpu:0'):
-    model = Sequential()
-    model.add(Dense(256, input_shape=(512,), activation='relu'))
-    model.add(Dense(128, input_shape=(256,), activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(128, input_shape=(64,)))
-    model.add(Dense(NUM_CLASSES, activation='softmax'))
+    # build and train model
+    top_layer_model = Sequential()
+    top_layer_model.add(Dense(256, input_shape=(512,), activation='relu'))
+    top_layer_model.add(Dense(256, input_shape=(256,), activation='relu'))
+    top_layer_model.add(Dropout(0.5))
+    top_layer_model.add(Dense(128, input_shape=(256,)))
+    top_layer_model.add(Dense(NUM_CLASSES, activation='softmax'))
 
     adamax = Adamax()
 
-    model.compile(loss='categorical_crossentropy',
+    top_layer_model.compile(loss='categorical_crossentropy',
                   optimizer=adamax, metrics=['accuracy'])
 
-    model.fit(x_train_feature_map, y_train,
+    # train
+    top_layer_model.fit(x_train_feature_map, y_train,
               validation_data=(x_train_feature_map, y_train),
               nb_epoch=FLAGS.n_epochs, batch_size=FLAGS.batch_size)
-
-    score = model.evaluate(x_test_feature_map,
+    # Evaluate
+    score = top_layer_model.evaluate(x_test_feature_map,
                            y_test, batch_size=FLAGS.batch_size)
 
-    print("Result: {}".format(score))
+    print("After top_layer_model training (test set): {}".format(score))
 
+    # Merge two models and create the final_model_final_final
     inputs = Input(shape=(48, 48, 3))
     vg_output = vg(inputs)
-    model_predictions = model(vg_output)
+    model_predictions = top_layer_model(vg_output)
     final_model = Model(input=inputs, output=model_predictions)
     final_model.compile(loss='categorical_crossentropy',
                         optimizer=adamax, metrics=['accuracy'])
     final_model_score = final_model.evaluate(x_train_input,
                                              y_train, batch_size=FLAGS.batch_size)
-    print("final model  train score: {}".format(final_model_score))
+    print("Sanity check - final_model (train score): {}".format(final_model_score))
 
     final_model_score = final_model.evaluate(x_test_input,
                                              y_test, batch_size=FLAGS.batch_size)
-    print("final model  test score: {}".format(final_model_score))
+    print("Sanity check - final_model (test score): {}".format(final_model_score))
+    # config = final_model.get_config()
+    # weights = final_model.get_weights()
 
-    config = final_model.get_config()
-    weights = final_model.get_weights()
-    new_model = Model.from_config(config)
-    new_model.set_weights(weights)
+    # probably don't need to create a new model
+    # model_to_save = Model.from_config(config)
+    # model_to_save.set_weights(weights)
+    model_to_save = final_model
 
-    print("hi: ")
-    print(new_model.input)
+    print("Model input name: {}".format(model_to_save.input))
+    print("Model output name: {}".format(model_to_save.output))
 
-    export_path = 'fifHomeSavedModel'
-    builder = saved_model_builder.SavedModelBuilder(export_path)
-
-    signature = predict_signature_def(inputs={'images': new_model.input},
-                                      outputs={'scores': new_model.output})
-
+    # Save Model
+    builder = saved_model_builder.SavedModelBuilder(FLAGS.export_path)
+    signature = predict_signature_def(inputs={'images': model_to_save.input},
+                                      outputs={'scores': model_to_save.output})
     with K.get_session() as sess:
         builder.add_meta_graph_and_variables(sess=sess,
                                              tags=[tag_constants.SERVING],
